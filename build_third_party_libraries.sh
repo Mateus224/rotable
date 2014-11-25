b283983611dc02a88a8ff853dcd69ba76609d792
Paul Wichern <wichern@tzi.de>
2014-11-25 17:35:52 +0100

    compile third party libraries script

---
+++ build_third_party_libraries.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+# Install required packages
+REQUIRED="subversion git"
+for pkg in $REQUIRED; do
+    if ! dpkg --get-selections | grep -q "^$pkg[[:space:]]*install$" >/dev/null; then
+        apt-get -qq install $pkg
+    fi
+done
+
+# Create directory:
+if [ ! -d "third-party" ]; then
+  mkdir third-party
+fi
+cd third-party
+
+# wiringPi
+if [ ! -f "wiringPi/.COMPILED" ]; then
+  if [ ! -d "wiringPi/.git" ]; then
+    git clone git://git.drogon.net/wiringPi
+  fi
+  
+  cd wiringPi
+  git pull origin
+  ./build
+  touch .COMPILED
+  cd ..
+fi
+
+# google-breakpad
+if [ ! -f "google-breakpad-read-only/.COMPILED" ]; then
+  if [ ! -d "google-breakpad-read-only/.svn" ]; then
+    svn checkout http://google-breakpad.googlecode.com/svn/trunk/ google-breakpad-read-only
+    cd google-breakpad-read-only
+  else
+    cd google-breakpad-read-only
+    svn update
+  fi
+  
+  ./configure
+  make
+  touch .COMPILED
+fi
+
+
