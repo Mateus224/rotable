33200b75b74a977d339358314fdff997ddb78958
Kamil Łuczak <rotable@luczakweb.pl>
2015-03-29 09:51:20 +0200

    Fix sql command

--- server/sql-commands/clients_update.sql
+++ server/sql-commands/clients_update.sql
@@ -1,1 +1,1 @@
-UPDATE `%1clients` SET `name` = :name, `mac_address` = :mac_address, `type` = :type, `date_last_change` = now() WHERE `id` = %2;
+UPDATE `%1clients` SET `name` = :name, `mac_address` = :mac_address, `type` = :type, `date_last_change` = now WHERE `id` = %2;
--- server/sql-commands/orderitems_update.sql
+++ server/sql-commands/orderitems_update.sql
@@ -1,1 +1,1 @@
-UPDATE `%1order_items` SET `order_id` = :order_id, `product_id` = :product_id, `amount` = :amount, `date_last_change` = now() WHERE `id` = %2;
+UPDATE `%1order_items` SET `order_id` = :order_id, `product_id` = :product_id, `amount` = :amount, `date_last_change` = now WHERE `id` = %2;
