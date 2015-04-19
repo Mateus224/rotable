2daec23ef15a849f0138be46ef4795239fce483a
Kamil Łuczak <rotable@luczakweb.pl>
2015-04-19 15:49:05 +0200

    Add ScheduleWorker class, prepare method to add/remove operation to/from schedule

--- server/include/schedule.h
+++ server/include/schedule.h
@@ -7,6 +7,10 @@
 #include <QThread>
 #endif
 
+#ifndef QTIMER_H
+#include <QTimer>
+#endif
+
 //------------------------------------------------------------------------------
 
 namespace rotable {
@@ -20,17 +24,46 @@ class rotable::Schedule : public QThread
     Q_OBJECT
 
 public:
+    /**
+     * Default constructor
+     *
+     * Init _timer interval, connect slots, etc.
+     */
     inline Schedule();
 
+    /**
+     * Set interval beetween refresh
+     * @param ms
+     */
     inline void setRefreshTime(int ms){ _timer.setInterval(ms); }
-    inline int refresh_time(){return _timer.interval();}
+
+    /**
+     * Get interval beetween refresh
+     * @return
+     */
+    inline int refresh_time(){ return _timer.interval(); }
+
+    void addOperiationToSchedule();
+    void removeOperiationFromSchedule();
 
 private:
    void stop();
    QTimer _timer;
    void run();
-};
 
+   class ScheduleWorker;
+};//class Schedule
+
+//------------------------------------------------------------------------------
+
+class rotable::Schedule::ScheduleWorker
+{
+    Q_OBJECT
+public:
+    void checkSchedule();
+};//class ScheduleWorker
+
+//------------------------------------------------------------------------------
 
 #endif // SCHEDULE
 
