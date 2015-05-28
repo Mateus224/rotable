a3d6a62964dd9b9f9c29f67a4b9e63be69c1db66
Kamil Łuczak <rotable@luczakweb.pl>
2015-05-28 22:15:40 +0200

    Modify order query

--- server/sql-commands/orders_insert.sql
+++ server/sql-commands/orders_insert.sql
@@ -1,3 +1,3 @@
 INSERT INTO `%1orders` 
-(`id`, `state`, `client_id`, `date_added`, `date_last_change`)
-VALUES (%2, %3, %4, 'now', 'now');
+(`id`, `state`, `income_id`, `client_id`, `date_added`, `date_last_change`)
+VALUES (%2, %3, %4, %5, 'now', 'now');
