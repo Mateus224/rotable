2a1c17ef3d16a30e1fd6a987879e42aed69a1b66
Kamil Łuczak <rotable@luczakweb.pl>
2015-07-14 09:46:30 +0200

    Fix sql command

--- server/sql-commands/orders_insert.sql
+++ server/sql-commands/orders_insert.sql
@@ -1,3 +1,3 @@
 INSERT INTO `%1orders` 
 (`id`, `state`, `income_id`, `client_id`, `date_added`, `date_last_change`)
-VALUES (%2, %3, %4, %5, 'now', 'now');
+VALUES (%2, %3, %4, %5, date('now'), date('now'));
