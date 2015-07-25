9bd0b18fdd398bce0b749868ed545d6b6bde81cb
Kamil Łuczak <rotable@luczakweb.pl>
2015-07-25 12:43:34 +0200

    Add comment in table.cpp

--- shared/source/tabele.cpp
+++ shared/source/tabele.cpp
@@ -5,14 +5,16 @@ using rotable::Table;
 
 Table::~Table()
 {
+    // Delete all orders
     qDeleteAll(_orders.begin(), _orders.end());
-    _orders.clear();     //Clear list
+    _orders.clear();     //Clear map
 }
 
 //------------------------------------------------------------------------------
 
 Table::Table(): rotable::Client()
 {
+    // Set default value
     _change = false;
     _waiterIsNeeded = false;
 }
@@ -20,9 +22,13 @@ Table::Table(): rotable::Client()
 //------------------------------------------------------------------------------
 
 void Table::updateOrder(Order* order){
+    // Check if order contains order
     if(_orders.contains(order->id()))
+        //Clear memory
         delete _orders[order->id()];
+    // Add order
     _orders[order->id()] = order;
+    //Change are made, set change value on true
     _change = true;
     //emit orderChanged();
 }
@@ -30,14 +36,18 @@ void Table::updateOrder(Order* order){
 //------------------------------------------------------------------------------
 void Table::updateTableStatus(const rotable::Table *table)
 {
+    // Change value create
     bool change = false;
 
+    // Chaeck if something change
     if(_waiterIsNeeded != table->_waiterIsNeeded)
     {
+        // Change so we set change
         change = true;
         _waiterIsNeeded = table->_waiterIsNeeded;
     }
 
+    // We do logic OR
     _change = _change || change;
 }
 //------------------------------------------------------------------------------
