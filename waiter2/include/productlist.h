680e6d802e05d2ae368a2b26978a8625e1522182
Kamil <gitlab@luczakweb.pl>
2015-08-17 12:29:05 +0200

    Implement public slot in ProductList

--- waiter2/include/productlist.h
+++ waiter2/include/productlist.h
@@ -13,13 +13,48 @@ namespace rotable{
 #include <QObject>
 #endif
 
+#ifndef QHASH_H
+#include <QHash>
+#endif
+
+#ifndef QSTRING_H
+#include <QString>
+#endif
+
+#include "product.h"
+
 //-----------------------------------------------------
 
 class rotable::ProductList: QObject
 {
     Q_OBJECT
 public:
+
+    //-----------------------------------------------------
+    // Default destructor and construcor
+    //-----------------------------------------------------
+
+    /**
+     * Default constructor
+     *
+     * @param parent        Parent object
+     */
     ProductList(QObject *parent = 0);
+
+signals:
+    void productNameChanged();
+
+public slots:
+    /**
+     * Get name of product
+     *
+     * @param idx           Produt id
+     * @return              Product name in QString
+     */
+    QString productName(const int &idx) const;
+
+private:
+    QHash<int, QString> _productList;
 };
 
 //-----------------------------------------------------
--- waiter2/source/productlist.cpp
+++ waiter2/source/productlist.cpp
@@ -12,3 +12,16 @@ rotable::ProductList::ProductList(QObject *parent): QObject(parent)
 }
 
 //-----------------------------------------------------
+
+QString ProductList::productName(const int &idx) const
+{
+    if(_productList.contains(idx))
+        return _productList[idx];
+    else
+    {
+        qCritical() << "Product don't exist";
+        return QString("");
+    }
+}
+
+//-----------------------------------------------------
