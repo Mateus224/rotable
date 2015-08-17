fe762023b9c5de8bf3dee45e39d855f40910702f
Kamil <gitlab@luczakweb.pl>
2015-08-17 12:42:09 +0200

    Implement method to add / update / remove product from list

--- waiter2/include/productlist.h
+++ waiter2/include/productlist.h
@@ -41,8 +41,35 @@ public:
      */
     ProductList(QObject *parent = 0);
 
+    //-----------------------------------------------------
+    // Method for waiter_client
+    //-----------------------------------------------------
+
+    /**
+     * Add product to list
+     *
+     * @param product       Product object
+     */
+    void addProduct(const rotable::Product *product);
+
+    /**
+     * Remove product from list
+     *
+     * @param id            Product id
+     * @return              true on success, false when product not exist
+     */
+    bool removeProduct(const int &id);
+
+    /**
+     * Update product name
+     *
+     * @param product       Product object
+     * @return              true on success, false when product not exist
+     */
+    bool updateProduct(const rotable::Product *product);
+
 signals:
-    void productNameChanged();
+    void productNameChanged(int id);
 
 public slots:
     /**
--- waiter2/source/productlist.cpp
+++ waiter2/source/productlist.cpp
@@ -13,6 +13,40 @@ rotable::ProductList::ProductList(QObject *parent): QObject(parent)
 
 //-----------------------------------------------------
 
+void ProductList::addProduct(const Product *product)
+{
+    _productList[product->id()] = product->name();
+}
+
+//-----------------------------------------------------
+
+bool ProductList::removeProduct(const int &id)
+{
+    if(_productList.contains(id))
+    {
+        _productList.remove(id);
+        return true;
+    }
+    else
+        return false;
+}
+
+//-----------------------------------------------------
+
+bool ProductList::updateProduct(const Product *product)
+{
+    if(_productList.contains(product->id()))
+    {
+        _productList[product->id()] = product->name();
+        emit productNameChanged(product->id());
+        return true;
+    }
+    else
+        return false;
+}
+
+//-----------------------------------------------------
+
 QString ProductList::productName(const int &idx) const
 {
     if(_productList.contains(idx))
