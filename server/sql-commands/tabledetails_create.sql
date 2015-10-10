55099d4be1f8dba64bc2688cfc59120fd2a5011c
Kamil Łuczak <rotable@luczakweb.pl>
2015-10-10 20:22:07 +0200

    Fix create sql command, add some code for realized connect and disconnect table

--- server/sql-commands/tabledetails_create.sql
+++ server/sql-commands/tabledetails_create.sql
@@ -1,4 +1,5 @@
-CREATE TABLE IF NOT EXISTS `%1table_details (
+CREATE TABLE IF NOT EXISTS `%1table_details`
+(
   `id` INTEGER PRIMARY KEY 	AUTOINCREMENT,
   `waiterIsNeeded` BOOLEAN NOT NULL,
   `connected` BOOLEAN NOT NULL,
