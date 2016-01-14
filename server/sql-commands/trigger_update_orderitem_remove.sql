2f4b2e4ab7f812816475b726a093920c14e3994c
Kamil Łuczak <rotable@luczakweb.pl>
2016-01-14 21:36:18 +0100

    Fix sql command

--- server/sql-commands/trigger_update_orderitem_remove.sql
+++ server/sql-commands/trigger_update_orderitem_remove.sql
@@ -1,6 +1,8 @@
-
 CREATE TRIGGER IF NOT EXISTS `%1update_orderitem_status_remove` UPDATE OF status ON `%1order_items` 
 WHEN old.status = 1 
 BEGIN
-	UPDATE `%1daily_incomes` SET income = income - new.price WHERE id = (select income_id from `%1orders` where id = new.id);
+    WHEN new.status != 1
+    BEGIN
+        UPDATE `%1daily_incomes` SET income = income - new.price WHERE id = (select income_id from `%1orders` where id = new.id);
+    END;
 END;
