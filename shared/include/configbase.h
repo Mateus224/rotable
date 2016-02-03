453372604e99c2aaa8c84ef12b4585a1c5cb68de
Kamil Łuczak <rotable@luczakweb.pl>
2016-02-03 21:23:40 +0100

    Modify configbase for use QSettings

--- shared/include/configbase.h
+++ shared/include/configbase.h
@@ -3,12 +3,16 @@
 
 //------------------------------------------------------------------------------
 
-#ifndef QMAP_H
-#include <QMap>
-#endif
+//#ifndef QMAP_H
+//#include <QMap>
+//#endif
 
-#ifndef QVARIANT_H
-#include <QVariant>
+//#ifndef QVARIANT_H
+//#include <QVariant>
+//#endif
+
+#ifndef QSETTINGS_H
+#include <QSettings>
 #endif
 
 //------------------------------------------------------------------------------
@@ -32,101 +36,112 @@ namespace rotable {
 
 //------------------------------------------------------------------------------
 
-/**
- * Base class for configuration loader classes.
- */
-class rotable::ConfigBase : public QObject
+class rotable::ConfigBase: public QSettings
 {
-  Q_OBJECT
-
-  Q_PROPERTY(QString path READ path NOTIFY pathChanged)
-  Q_PROPERTY(QString errorStr READ errorStr NOTIFY errorChanged)
-
 public:
-  /**
-   * Default constructor
-   *
-   * @param parent    parent element
-   */
-  ConfigBase(QObject* parent = 0);
-
-  /**
-   * Constructor
-   *
-   * @param path      path to config file
-   *                  (see error() for errors)
-   * @param parent    parent element
-   */
-  ConfigBase(const QString& path, QObject* parent = 0);
-
-  /**
-    * Destructor
-    */
-  ~ConfigBase();
-
-  /**
-   * Load configuration file.
-   *
-   * @param path      path to config file
-   * @return          e_config_no_error on success
-   */
-  ConfigError load(const QString& path);
-
-  /**
-   * Save configuration to file.
-   *
-   * @param path      destination file path (optional, will use input path)
-   * @return          e_config_no_error on success
-   */
-  ConfigError save(const QString& path = QString());
-
-  /**
-   * Get last error.
-   *
-   * @return          last error
-   */
-  ConfigError error() const { return _error; }
-
-  /**
-   * Get config file path.
-   *
-   * @return          config file path
-   */
-  inline const QString& path() const { return _path; }
-
-  /**
-   * Get last error as a string.
-   *
-   * @return          last error string
-   */
-  QString errorStr() const;
-
-signals:
-  void errorChanged();
-  void pathChanged();
-
-protected:
-  virtual void loaded() {}
-
-  /** Key-Value map */
-  QMap<QString, QVariant> _values;
-
-private:
-  /**
-   * Set current error.
-   *
-   * @param error     new error
-   * @return          new error
-   */
-  ConfigError setError(ConfigError error);
-
-  /** Path to configuration file */
-  QString _path;
-
-  /** Last error */
-  ConfigError _error;
-}; // class ConfigBase
+    ConfigBase(const QString &path);
+};
 
 //------------------------------------------------------------------------------
+
+
+////------------------------------------------------------------------------------
+
+///**
+// * Base class for configuration loader classes.
+// */
+//class rotable::ConfigBase : public QObject
+//{
+//  Q_OBJECT
+
+//  Q_PROPERTY(QString path READ path NOTIFY pathChanged)
+//  Q_PROPERTY(QString errorStr READ errorStr NOTIFY errorChanged)
+
+//public:
+//  /**
+//   * Default constructor
+//   *
+//   * @param parent    parent element
+//   */
+//  ConfigBase(QObject* parent = 0);
+
+//  /**
+//   * Constructor
+//   *
+//   * @param path      path to config file
+//   *                  (see error() for errors)
+//   * @param parent    parent element
+//   */
+//  ConfigBase(const QString& path, QObject* parent = 0);
+
+//  /**
+//    * Destructor
+//    */
+//  ~ConfigBase();
+
+//  /**
+//   * Load configuration file.
+//   *
+//   * @param path      path to config file
+//   * @return          e_config_no_error on success
+//   */
+//  ConfigError load(const QString& path);
+
+//  /**
+//   * Save configuration to file.
+//   *
+//   * @param path      destination file path (optional, will use input path)
+//   * @return          e_config_no_error on success
+//   */
+//  ConfigError save(const QString& path = QString());
+
+//  /**
+//   * Get last error.
+//   *
+//   * @return          last error
+//   */
+//  ConfigError error() const { return _error; }
+
+//  /**
+//   * Get config file path.
+//   *
+//   * @return          config file path
+//   */
+//  inline const QString& path() const { return _path; }
+
+//  /**
+//   * Get last error as a string.
+//   *
+//   * @return          last error string
+//   */
+//  QString errorStr() const;
+
+//signals:
+//  void errorChanged();
+//  void pathChanged();
+
+//protected:
+//  virtual void loaded() {}
+
+//  /** Key-Value map */
+//  QMap<QString, QVariant> _values;
+
+//private:
+//  /**
+//   * Set current error.
+//   *
+//   * @param error     new error
+//   * @return          new error
+//   */
+//  ConfigError setError(ConfigError error);
+
+//  /** Path to configuration file */
+//  QString _path;
+
+//  /** Last error */
+//  ConfigError _error;
+//}; // class ConfigBase
+
+////------------------------------------------------------------------------------
 
 #endif // ROTABLE_CONFIGBASE_H
