45a3fa27897d35c71b3592da867c978b86563742
Kamil Łuczak <rotable@luczakweb.pl>
2016-02-06 19:26:06 +0100

    Change constructor for Waiter_Client

--- waiter2/source/waiter_client.cpp
+++ waiter2/source/waiter_client.cpp
@@ -19,35 +19,35 @@ using namespace rotable;
 
 Waiter_Client::Waiter_Client(const QString &configFilePath, QObject *parent)
   : QObject(parent),
-    _accepted(false), _state("DISCONNECTED"), _stopping(false)
+    _accepted(false), _state("DISCONNECTED"), _stopping(false), _config(configFilePath)
 {
-    ConfigError error = _config.load(configFilePath);
 
-    switch (error) {
-    case FileNotLoaded:
-      qCritical() << tr("FATAL: Could not load %1").arg(configFilePath);
-      exit(EXIT_FAILURE);
-      break;
-    case FileNotFound:
-      qCritical() << tr("FATAL: File not found: %1").arg(configFilePath);
-      exit(EXIT_FAILURE);
-      break;
-    case FileNoReadAccess:
-      qCritical() << tr("FATAL: No read access on %1").arg(configFilePath);
-      exit(EXIT_FAILURE);
-      break;
-    case FileNoWriteAccess:
-      qCritical() << tr("FATAL: No write access on %1").arg(configFilePath);
-      exit(EXIT_FAILURE);
-      break;
-    case FileParseError:
-      qCritical() << tr("FATAL: Error parsing %1").arg(configFilePath);
-      exit(EXIT_FAILURE);
-      break;
-    default:
-      // everything is fine :)
-      break;
-    }
+//  Always work, not need
+//    switch (error) {
+//    case FileNotLoaded:
+//      qCritical() << tr("FATAL: Could not load %1").arg(configFilePath);
+//      exit(EXIT_FAILURE);
+//      break;
+//    case FileNotFound:
+//      qCritical() << tr("FATAL: File not found: %1").arg(configFilePath);
+//      exit(EXIT_FAILURE);
+//      break;
+//    case FileNoReadAccess:
+//      qCritical() << tr("FATAL: No read access on %1").arg(configFilePath);
+//      exit(EXIT_FAILURE);
+//      break;
+//    case FileNoWriteAccess:
+//      qCritical() << tr("FATAL: No write access on %1").arg(configFilePath);
+//      exit(EXIT_FAILURE);
+//      break;
+//    case FileParseError:
+//      qCritical() << tr("FATAL: Error parsing %1").arg(configFilePath);
+//      exit(EXIT_FAILURE);
+//      break;
+//    default:
+//      // everything is fine :)
+//      break;
+//    }
 
     _products = new ProductContainer();
     //set container for productlist(connect sigals-slots, etc.)
