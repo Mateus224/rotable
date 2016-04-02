9bb62b5a6cbb83783e234b3a7a136577691199cf
Kamil Łuczak <rotable@luczakweb.pl>
2016-04-02 10:53:20 +0200

    Generate QString with data and hash

--- LicenceGenerator/mainwindow.cpp
+++ LicenceGenerator/mainwindow.cpp
@@ -1,6 +1,9 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QCryptographicHash>
+#include <QTextCodec>
+
 MainWindow::MainWindow(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::MainWindow)
@@ -17,4 +20,15 @@ void MainWindow::on_hostNameLineEdit_textChanged(const QString &arg1)
 {
     Q_UNUSED(arg1);
     ui->pushButton->setEnabled(ui->hostNameLineEdit->text().trimmed().length() != 0);
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    QString dataString;
+    dataString = ui->hostNameLineEdit->text() + ";" + ui->platformComboBox->currentText() + ";" + QString::number(ui->tableCountSpinBox->value());
+
+    QString hash = QTextCodec::codecForName("UTF-8")->toUnicode(QCryptographicHash::hash(dataString.toUtf8(), QCryptographicHash::Sha3_512).toHex());
+
+    QString tmp = dataString + ";;" + hash;
+    ui->plainTextEdit->setPlainText(tmp);
 }
--- LicenceGenerator/mainwindow.h
+++ LicenceGenerator/mainwindow.h
@@ -18,6 +18,8 @@ public:
 private slots:
     void on_hostNameLineEdit_textChanged(const QString &arg1);
 
+    void on_pushButton_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
