9d72c442588b72d12786f8e9abc9d2e49ea47c14
Kamil Łuczak <rotable@luczakweb.pl>
2016-06-28 15:33:22 +0200

    Add property version to script

--- server/sql-commands/update-database/0.0.3.sql
+++ server/sql-commands/update-database/0.0.3.sql
@@ -1,5 +1,5 @@
+UPDATE `%1configs` SET `value` = "0.0.3" WHERE `name` = "2";;
 ALTER TABLE `%1products` ADD `sequence` INTEGER NOT NULL DEFAULT 0;;
---ALTER TABLE `%1categories` ADD `sequence_last` INTEGER NOT NULL DEFAULT 0;;
 UPDATE `%1products` SET `sequence` = (
     SELECT COUNT(*) FROM `%1products` as `p1` WHERE
     `removed` = 0 and `p1`.`category_id` = `rotable_products`.`category_id` and `p1`.`id` <= `rotable_products`.`id`
