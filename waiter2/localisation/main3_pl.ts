b6c5eaae1173c4d4976b18a85dde752ad4f194e4
AdrianJab <adrian.jablonskiii@gmail.com>
2016-08-12 20:38:18 +0200

    Simple language support - loads language based on system language. (Only for waiter for now). Prototype language class - shared.
    Former-commit-id: a59bb98d95d6a0fe46f6ae51d4c85439fc412f45 [formerly 8e96f37b8f21ca4e2ec9e3575b05ccf6aac80da9 [formerly f08f0907612d5af9fe03bad7abd2041676351f1e]]
    Former-commit-id: ad4e3c1e4a9814e687a8dfc3fd640d4c51999acf
    Former-commit-id: 2dfb2e8baaf8e3d5dd8f9cd5049f155109b8d711

---
+++ waiter2/localisation/main3_pl.ts
@@ -0,0 +1,57 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pl_PL">
+<context>
+    <name>main3</name>
+    <message>
+        <location filename="../qml/waiter/main3.qml" line="9"/>
+        <source>Waiter client</source>
+        <translation type="unfinished">Kelner - Klient</translation>
+    </message>
+    <message>
+        <location filename="../qml/waiter/main3.qml" line="45"/>
+        <source>Menu</source>
+        <translation type="unfinished">Menu</translation>
+    </message>
+    <message>
+        <location filename="../qml/waiter/main3.qml" line="52"/>
+        <source>Log out</source>
+        <translation type="unfinished">Wyloguj</translation>
+    </message>
+    <message>
+        <location filename="../qml/waiter/main3.qml" line="56"/>
+        <source>Settings</source>
+        <translation type="unfinished">Ustawienia</translation>
+    </message>
+    <message>
+        <location filename="../qml/waiter/main3.qml" line="60"/>
+        <source>Exit</source>
+        <translation type="unfinished">Wyjdź</translation>
+    </message>
+    <message>
+        <location filename="../qml/waiter/main3.qml" line="86"/>
+        <source>Table list</source>
+        <translation type="unfinished">Lista stołów</translation>
+    </message>
+    <message>
+        <location filename="../qml/waiter/main3.qml" line="175"/>
+        <source>Bill on table: </source>
+        <translation type="unfinished">Zamówienie na stole: </translation>
+    </message>
+    <message>
+        <location filename="../qml/waiter/main3.qml" line="252"/>
+        <source>To pay: </source>
+        <translation type="unfinished">Do zapłaty: </translation>
+    </message>
+    <message>
+        <location filename="../qml/waiter/main3.qml" line="256"/>
+        <source>Select all</source>
+        <translation type="unfinished">Zaznacz wszystkie</translation>
+    </message>
+    <message>
+        <location filename="../qml/waiter/main3.qml" line="266"/>
+        <source>Table need waiter</source>
+        <translation type="unfinished">Stół potrzebuje kelnera</translation>
+    </message>
+</context>
+</TS>
