488f4d9a9bda7a3b672d69ad6ff41e208d94d6e8
Kamil Łuczak <gitlab@luczakweb.pl>
2016-08-18 20:58:51 +0200

    Set new version in database
    Former-commit-id: a2ecc49d3a9c531bff626c3199ca383cb6380051

--- server/sql-commands/update-database/0.0.4.sql
+++ server/sql-commands/update-database/0.0.4.sql
@@ -1,3 +1,4 @@
+UPDATE `%1configs` SET `value` = "0.0.4" WHERE `name` = "2";;
 CREATE TABLE IF NOT EXISTS `%1waitercategories` (
   `waiter_id` INTEGER PRIMARY KEY,
   `category_id` INTEGER PRIMARY KEY
