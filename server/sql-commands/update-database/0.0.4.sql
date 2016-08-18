2e8058a818052bdb03e0327e9f12b342e1ddfd13
Kamil Łuczak <gitlab@luczakweb.pl>
2016-08-18 21:56:46 +0200

    Add list with categories to waiter, fix sql error

--- server/sql-commands/update-database/0.0.4.sql
+++ server/sql-commands/update-database/0.0.4.sql
@@ -1,5 +1,5 @@
 UPDATE `%1configs` SET `value` = "0.0.4" WHERE `name` = "2";;
 CREATE TABLE IF NOT EXISTS `%1waitercategories` (
-  `waiter_id` INTEGER PRIMARY KEY,
-  `category_id` INTEGER PRIMARY KEY
+  `waiter_id` INTEGER,
+  `category_id` INTEGER
 );;
