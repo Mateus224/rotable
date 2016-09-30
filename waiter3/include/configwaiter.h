3ff317d49236848de435c6eb538537be886a2a61
AdrianJab <adrian.jablonskiii@gmail.com>
2016-09-30 14:39:33 +0200

    Create new project for new waiter.
    Temporary fix for server not compiling.
    
    Former-commit-id: 7c8e3d6de64fddc765bd4dc2d6697ac55f02a3f9 [formerly a95c32d74d0d4979b8233141d069a6818b1896ee [formerly cc27d2e1555a2f1d2e8a5e4f1f1002a7a0037cf7]]
    Former-commit-id: 0eea9794b92d7f0e62a2e89a3b56776431c9a280
    Former-commit-id: 45e06b42ad0add20c94fb1de36e1a413332df953

---
+++ waiter3/include/configwaiter.h
@@ -0,0 +1,63 @@
+#ifndef ROTABLE_CONFIGWAITER_H
+#define ROTABLE_CONFIGWAITER_H
+
+//------------------------------------------------------------------------------
+
+#ifndef QSTRING_H
+#include <QString>
+#endif
+
+#include "configbase.h"
+
+//------------------------------------------------------------------------------
+
+namespace rotable {
+  class ConfigWaiter;
+}
+
+//------------------------------------------------------------------------------
+
+class rotable::ConfigWaiter : public ConfigBase
+{
+  Q_OBJECT
+
+  Q_PROPERTY(int port READ port NOTIFY portChanged)
+  Q_PROPERTY(QString serverAddress READ serverAddress NOTIFY serverAddressChanged)
+  Q_PROPERTY(QString clientName READ clientName NOTIFY clientNameChanged)
+
+public:
+  /**
+   * Constructor.
+   *
+   * @param path      path to config file
+   *                  (see error() for errors)
+   * @param parent    parent element
+   */
+  ConfigWaiter(const QString& path, QObject* parent = 0);
+
+  // Getters
+  inline const QString& serverAddress() const { return _serverAddress; }
+  inline int port() const { return _port; }
+  inline const QString& clientName() const { return _clientName; }
+
+signals:
+  void portChanged();
+  void serverAddressChanged();
+  void clientNameChanged();
+
+private:
+  /**
+   * Configuration has been loaded
+   */
+  virtual void loaded();
+
+  void initData();
+
+  int _port;
+  QString _serverAddress;
+  QString _clientName;
+}; // class ConfigWaiter
+
+//------------------------------------------------------------------------------
+
+#endif // ROTABLE_CONFIGWAITER_H
